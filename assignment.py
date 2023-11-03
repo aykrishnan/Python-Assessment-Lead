6965c5b761d72d39827093baa842b6423003e7fe
Peter Lane <Peter.Lane@ama-assn.org>
2023-11-03 14:33:14 -0500

    Add clarifying comment.

--- assignment.py
+++ assignment.py
@@ -2,6 +2,7 @@
 import task
 
 
+# Make changes to the Widget class as needed
 class Widget:
     def run(self, parameters):
         return [f'{parameters["first_name"]} {parameters["last_name"]}'.encode()]
